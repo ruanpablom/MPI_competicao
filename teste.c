5d34b9496c1cf606e4869635115dff9491dab247
ruan <ruan@udesc>
2015-11-10 17:16:05 -0200

    alterado o quebrado

--- teste.c
+++ teste.c
@@ -2,6 +2,7 @@
 #include<string.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include <crypt.h>
 #define P_T 10
 #define D_T 64
 
@@ -10,27 +11,30 @@ int main(int argc, char **argv){
     char d[] = {"./abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"};
     int *pass=NULL;
 	char *c_pass=NULL;
-	char pass_s[14];
+	char pass_s[15];
 	char *salt;
   	char *result;
   	int ok;
 	FILE *f;
 
-	f = fopen(argv[1],"rt");	
+	salt = (char*)malloc(2*sizeof(char));	
 
+	f = fopen(argv[1],"r");	
 	for(i = 1 ; i < P_T ; i++){
-		fscanf(f,"%s",pass_s);
+		fscanf(f,"%s",pass_s);	
 		memcpy(salt,pass_s,2);
+		printf("%s\n",salt);
 		if(pass!=NULL) free(pass);
 		if(c_pass!=NULL) free(c_pass);
     	pass = (int*)malloc(i*sizeof(int));
 		c_pass = (char*)malloc(i*sizeof(char));
+		
 		while(j!=-1){
 		    for(j = 0 ; j < D_T ; j++){ 
 		        pass[P_T - 1] = j;
 		        for(i = 0 ; i < P_T ; i++) sprintf(c_pass,"%c",d[pass[i]]);
 				result = crypt(c_pass, salt);
-				ok = strcmp (result, pass_s) == 0;
+				ok = strcmp (result, pass_s);
 				if(ok){
 					printf("é NoIx porra! A senha é: %s\n", c_pass);
 					break;
